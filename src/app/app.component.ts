cebfc721316791283900e2fb042b2b33ef8bc4d5
Mrutyunjaya <me.mrutyunjaya@gmail.com>
2022-04-30 10:01:43 +0530

    emp details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-employee';
+export class AppComponent implements OnInit {
+
+  login: boolean = false;
+  fullname: string;
+  tokenno: string;
+
+  ngOnInit() {
+    this.fullname = localStorage.getItem("name");
+    this.tokenno = localStorage.getItem("tno");
+    if ((this.fullname != "") && (this.fullname != null)) {
+      this.login = true;
+    }
+  }
+
 }
